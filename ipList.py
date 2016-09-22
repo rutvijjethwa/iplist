ed06bf5f94870eb57cf5d76dfe98071f183bfe86
Rutvij Jethwa <rutvijjethwa@gmail.com>
2016-09-22 15:47:51 +0530

    Update ipList.py

--- ipList.py
+++ ipList.py
@@ -5,7 +5,9 @@ ipconfigCmdReg = re.compile(r'(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})')
 outputx = ipconfigCmdReg.findall(ipconfigCmd)
 ipList = []
 for i in range(len(outputx)):
-    if int(outputx[i][0]) < 224:
+    if int(outputx[i][3]) == 255:
+        continue
+    elif int(outputx[i][0]) < 224:
         ipList.append((str('.'.join(outputx[i]))))
 print("List of active IP's in your network :")
 for i in ipList:
