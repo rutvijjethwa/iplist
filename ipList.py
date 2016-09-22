e4e7718f7b6b6b78be8e988a158611e0521e35cd
Rutvij Jethwa <rutvijjethwa@gmail.com>
2016-09-22 15:23:13 +0530

    Add files via upload

---
+++ ipList.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import subprocess , re
+ipconfigCmd = str(subprocess.check_output("arp -a"))
+ipconfigCmdReg = re.compile(r'(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})')
+outputx = ipconfigCmdReg.findall(ipconfigCmd)
+ipList = []
+for i in range(len(outputx)):
+    if int(outputx[i][0]) < 224:
+        ipList.append((str('.'.join(outputx[i]))))
+print("List of active IP's in your network :")
+for i in ipList:
+    print(i)
