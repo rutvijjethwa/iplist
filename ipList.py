fe1cb03c4a6510a2bd3cd0f11e4dfb5a02e227be
Rutvij Jethwa <rutvijjethwa@gmail.com>
2016-09-24 11:38:13 +0530

    Update ipList.py

--- ipList.py
+++ ipList.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 import subprocess , re
+from prettytable import PrettyTable as pt
 ipconfigCmd = str(subprocess.check_output("arp -a"))
-ipconfigCmdReg = re.compile(r'(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})')
+ipconfigCmdReg = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+([0-9a-fA-F]{2}-[0-9a-fA-F]{2}-[0-9a-fA-F]{2}-[0-9a-fA-F]{2}-[0-9a-fA-F]{2}-[0-9a-fA-F]{2})')
 outputx = ipconfigCmdReg.findall(ipconfigCmd)
-ipList = []
+tempRegS = re.compile(r'\d{3}')
+tempRegE = re.compile(r'255$')
+ipListDict = {}
 for i in range(len(outputx)):
-    if int(outputx[i][3]) == 255:
+    c1 = True if int(tempRegS.search(str(outputx[i][0])).group(0)) < 224 else False
+    c2 = True if tempRegE.search(str(outputx[i][0])) == None else False
+    if c1 and c2:
+        ipListDict[outputx[i][0]]= outputx[i][1]
+    else:
         continue
-    elif int(outputx[i][0]) < 224:
-        ipList.append((str('.'.join(outputx[i]))))
-print("List of active IP's in your network :")
-for i in ipList:
-    print(i)
+x = pt(["IP Address","MAC Address"])
+x.align["IP Address"] = "l"
+for i in ipListDict:
+    x.add_row([i,ipListDict[i]])
+print(x)
